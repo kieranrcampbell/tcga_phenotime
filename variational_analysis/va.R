1f951adf8c5b9ce312e0f6989e39011224cc2044
Kieran Campbell <kieranrcampbell@gmail.com>
2016-11-30 14:24:35 +0000

    ready for gene level analysis

--- variational_analysis/va.R
+++ variational_analysis/va.R
@@ -4,17 +4,17 @@ library(Rcpp)
 source("../cavi/phenotime_cavi.R")
 sourceCpp("../cavi/cavi.cpp")
 
-load("../data/sc_coad_tumour_only.Rdata")
+load("../data/sc_coad_gene.Rdata")
 
 scale_vec <- function(x) (x - mean(x)) / sd(x)
 
 # Select genes ------------------------------------------------------------
 
-var_exprs <- matrixStats::rowVars(exprs(sc_tumour))
+var_exprs <- matrixStats::rowVars(exprs(sc_tumour_gene))
 
 to_use <- var_exprs > 1
 
-sc <- sc_tumour[to_use, ]
+sc <- sc_tumour_gene[to_use, ]
 
 
 # Construct covariates ----------------------------------------------------
@@ -32,7 +32,7 @@ y <- scale(t(exprs(sc)))
 
 # Call phenotime ----------------------------------------------------------
 
-pcavi <- phenotime_cavi(y, x, elbo_tol = 0.005)
+pcavi <- phenotime_cavi(y, x, elbo_tol = 0.005, thin = 2)
 
 # Save results
 retained_fnames <- featureNames(sc)
